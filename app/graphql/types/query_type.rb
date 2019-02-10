9dd0c908d58baae9167f122221448c372972c02e
Jose Neto <jose.neto@lavid.ufpb.br>
2019-02-10 18:24:30 -0300

    added source data for the database

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,10 +4,12 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
+    field :test_field, String, null: false, description: "An example field added by the generator"
+
     def test_field
-      "Hello World!"
+
+      text = open('app/assets/pingas/timetable.json').read
+      return JSON.parse(text)['time'][0]['id']
     end
   end
 end
