e28deaa313ff93ea949b33f522bf00bba7dad797
Jose Neto <jose.neto@lavid.ufpb.br>
2019-02-10 19:56:14 -0300

    initial database draft(in /db) and info fetch logic for seeding the db

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,15 +1,15 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
 
-    # TODO: remove me
     field :test_field, String, null: false, description: "An example field added by the generator"
 
     def test_field
 
-      text = open('app/assets/pingas/timetable.json').read
-      return JSON.parse(text)['time'][0]['id']
+      'Oi rs'
+
     end
+
+    #return JSON.parse(text)['time'][0]['turmas'][0]
   end
 end
+
